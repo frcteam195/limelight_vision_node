ee581bff603dba01148d69021656545ac71effb0
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-01-29 17:55:33 +0000

    Add limelight control

--- src/limelight_vision_node.cpp
+++ src/limelight_vision_node.cpp
@@ -1,11 +1,163 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+#include "network_tables_node/NTGetDouble.h"
+#include "network_tables_node/NTSetDouble.h"
+
+#include "limelight_vision_node/Limelight_Info.h"
+#include "limelight_vision_node/Limelight_Status.h"
+#include "limelight_vision_node/Limelight.h"
+#include "limelight_vision_node/Limelight_Control.h"
+
 #include <thread>
 #include <string>
 #include <mutex>
+#include <vector>
+
+#define STR_PARAM(s) #s
+#define CKSP(s) ckgp( STR_PARAM(s) )
+std::string ckgp(std::string instr)
+{
+	std::string retVal = ros::this_node::getName();
+	retVal += "/";
+	retVal += instr;
+	return retVal;
+}
+
 
 ros::NodeHandle* node;
+ros::ServiceClient nt_getdouble_client;
+ros::ServiceClient nt_setdouble_client;
+std::mutex limelightMutex;
+std::vector<std::string> limelight_names;
+
+ros::ServiceClient& getNTGetDoubleSrv()
+{
+	if (!nt_getdouble_client)
+	{
+		nt_getdouble_client = node->serviceClient<network_tables_node::NTGetDouble>("nt_getdouble", true);
+	}
+	return nt_getdouble_client;
+}
+
+ros::ServiceClient& getNTSetDoubleSrv()
+{
+	if (!nt_setdouble_client)
+	{
+		nt_setdouble_client = node->serviceClient<network_tables_node::NTSetDouble>("nt_setdouble", true);
+	}
+	return nt_setdouble_client;
+}
+
+void publish_limelight_data()
+{
+	static ros::Publisher limelight_pub = node->advertise<limelight_vision_node::Limelight_Status>("LimelightStatus", 1);
+	static limelight_vision_node::Limelight_Status limelightStatus;
+	//Transform to limelight from baselink
+	//Transform from limelight to target
+	//Take x from hub link transform
+	//Statically publish limelight tf2
+	//transformStamped.header.frame_id = "hub_link"; //Existing frame of reference
+	//transformStamped.child_frame_id = "hub_full_height";	//New frame
+
+	ros::Rate rate(100);
+
+	while (ros::ok())
+	{
+		{
+			std::lock_guard<std::mutex> lock(limelightMutex);
+			limelightStatus.limelights.clear();
+			ros::ServiceClient& nt_getdouble_localclient = getNTGetDoubleSrv();
+			if (nt_getdouble_localclient)
+			{
+				for (const std::string& s : limelight_names)
+				{
+					network_tables_node::NTGetDouble ntmsg;
+					ntmsg.request.table_name = s;
+					ntmsg.request.default_value = 0;
+
+					limelight_vision_node::Limelight_Info limelightInfo;
+					limelightInfo.name = s;
+					
+					ntmsg.request.entry_name = "tv";
+					ntmsg.response.output = 0;
+					nt_getdouble_localclient.call(ntmsg);
+					limelightInfo.target_valid = ntmsg.response.output > 0 ? true : false;
+
+					ntmsg.request.entry_name = "tx";
+					ntmsg.response.output = 0;
+					nt_getdouble_localclient.call(ntmsg);
+					limelightInfo.target_dx_deg = ntmsg.response.output;
+
+					ntmsg.request.entry_name = "ty";
+					ntmsg.response.output = 0;
+					nt_getdouble_localclient.call(ntmsg);
+					limelightInfo.target_dy_deg = ntmsg.response.output;
+
+					ntmsg.request.entry_name = "ta";
+					ntmsg.response.output = 0;
+					nt_getdouble_localclient.call(ntmsg);
+					limelightInfo.target_area = ntmsg.response.output;
+
+					ntmsg.request.entry_name = "ts";
+					ntmsg.response.output = 0;
+					nt_getdouble_localclient.call(ntmsg);
+					limelightInfo.target_skew = ntmsg.response.output;
+
+					ntmsg.request.entry_name = "tl";
+					ntmsg.response.output = 0;
+					nt_getdouble_localclient.call(ntmsg);
+					limelightInfo.target_latency = ntmsg.response.output;
+
+					limelightStatus.limelights.push_back(limelightInfo);
+				}
+			}
+			limelight_pub.publish(limelightStatus);
+		}
+
+		rate.sleep();
+	}
+}
+
+void limelightControlCallback(const limelight_vision_node::Limelight_Control& msg)
+{
+	ros::ServiceClient& nt_setdouble_localclient = getNTSetDoubleSrv();
+	if (nt_setdouble_localclient)
+	{
+		for (const limelight_vision_node::Limelight& ll : msg.limelights)
+		{
+			bool setSuccess = true;
+
+			network_tables_node::NTSetDouble ntmsg;
+			ntmsg.request.table_name = ll.name;
+
+			ntmsg.request.entry_name = "ledMode";
+			ntmsg.request.value = ll.ledMode;
+			setSuccess &= nt_setdouble_localclient.call(ntmsg);
+
+			ntmsg.request.entry_name = "camMode";
+			ntmsg.request.value = ll.camMode;
+			setSuccess &= nt_setdouble_localclient.call(ntmsg);
+
+			ntmsg.request.entry_name = "pipeline";
+			ntmsg.request.value = ll.pipeline;
+			setSuccess &= nt_setdouble_localclient.call(ntmsg);
+
+			ntmsg.request.entry_name = "stream";
+			ntmsg.request.value = ll.stream;
+			setSuccess &= nt_setdouble_localclient.call(ntmsg);
+
+			ntmsg.request.entry_name = "snapshot";
+			ntmsg.request.value = ll.snapshot;
+			setSuccess &= nt_setdouble_localclient.call(ntmsg);
+
+			if (!setSuccess)
+			{
+				ROS_WARN("Failed to set values for limelight: %s", ll.name.c_str());
+			}
+		}
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -22,8 +174,23 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "limelight_vision_node");
 
 	ros::NodeHandle n;
-
 	node = &n;
+
+	bool required_params_found = true;
+	required_params_found &= n.getParam(CKSP(limelight_names), limelight_names);
+	if (!required_params_found)
+	{
+		ROS_ERROR("Missing required parameters. Please check the list and make sure all required parameters are included");
+		return 1;
+	}
+
+	getNTGetDoubleSrv();
+	getNTSetDoubleSrv();
+
+	std::thread limelightSendThread(publish_limelight_data);
+	ros::Subscriber limelightControl = node->subscribe("LimelightControl", 100, limelightControlCallback);
+
+	limelightSendThread.join();
 
 	ros::spin();
 	return 0;
